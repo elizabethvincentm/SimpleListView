09894d0a0cd827010e847c3b66286d57b7eaf961
Elizabeth Vincent M <elizabethvincentm@gmail.com>
2020-06-16 15:45:53 +0530

    feat:add refresh logic

--- components/List.js
+++ components/List.js
@@ -1,21 +1,30 @@
 import React, { useContext } from 'react'
-import { View, Text, FlatList } from 'react-native'
+import { View, ActivityIndicator, FlatList, RefreshControl } from 'react-native'
 import { Card } from './Card'
 import { AppContext } from '../contexts'
 
 export const List = () => {
-  const { state } = useContext(AppContext)
-  const { status, data } = state
+  const { state, setState } = useContext(AppContext)
+  const { status, data, refreshing } = state
+
   return (
     <View>
       {status === 'success' && (
         <FlatList
           data={data}
           renderItem={({ item }) => <Card details={item} />}
+          refreshControl={
+            <RefreshControl
+              refreshing={refreshing}
+              onRefresh={() => setState({ ...state, refreshing: true })}
+            />
+          }
           keyExtractor={(item, index) => JSON.stringify(index)}
         />
       )}
-      {status === 'loading' && <Text>Loading</Text>}
+      {status === 'loading' && (
+        <ActivityIndicator size="large" color="dodgerblue" />
+      )}
     </View>
   )
 }

96540d9fa2221242fe4e2f65b8b6f0fbad180460
Elizabeth Vincent M <elizabethvincentm@gmail.com>
2020-06-16 15:01:51 +0530

    feat:use axios to fetch data

--- components/List.js
+++ components/List.js
@@ -10,7 +10,7 @@ export const List = () => {
     <View>
       {status === 'success' && (
         <FlatList
-          data={data.results}
+          data={data}
           renderItem={({ item }) => <Card details={item} />}
           keyExtractor={(item, index) => JSON.stringify(index)}
         />
